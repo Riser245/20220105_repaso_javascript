59228e216d504e3fcbc7e1e4fbbc213d22bd21e3
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-04-26 15:32:21 -0600

    Tercer Ejercicio

--- trabajo java/java.js
+++ trabajo java/java.js
@@ -59,12 +59,57 @@ const mostrarPokemons = () => {
   const contenido = document.getElementById('contenido');
   objetoPokemon.map(Pokemons => {
     contenido.innerHTML += `
-      <p class="card mt-5 border-2 border-black ">
-          nombre : ${Pokemons.nombre} <br>
-          entrada : ${Pokemons.entrada} <br>
-      </p>
+      <div class="card mt-5 border-1 border-black ">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">${Pokemons.nombre}</p>
+    <p class="card-text">${Pokemons.entrada}</p>
+  </div>
+</div>
       `;
   })
 }
 
+
 mostrarPokemons()
+
+
+
+//script sobre la api de una serie.
+
+
+async function fetchCharacters() {
+  try {
+      const response = await fetch('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=50');
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.log('Error fetching data:', error);
+  }
+}
+
+async function displayCharacters() {
+  const characters = await fetchCharacters();
+  const charactersList = document.getElementById('characters-list');
+  characters.forEach(character => {
+      const characterCard = document.createElement('div');
+      characterCard.classList.add('col');
+
+      characterCard.innerHTML = `
+          <div class="card h-100">
+              <div class="card-body">
+                  <h5 class="card-title">${character.name}</h5>
+                  <p class="card-text">Aliases: ${character.aliases.join(', ')}</p>
+                  <p class="card-text">Gender: ${character.gender}</p>
+                  <p class="card-text">Culture: ${character.culture}</p>
+                  <p class="card-text">Born: ${character.born}</p>
+              </div>
+          </div>
+      `;
+
+      charactersList.appendChild(characterCard);
+  });
+}
+
+// Llamada inicial para mostrar los personajes
+displayCharacters();
