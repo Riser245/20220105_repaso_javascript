57367daa33030ce5b4d4c07aab65c2f87f772feb
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-04-26 14:16:37 -0600

    segundo ejercicio

--- trabajo java/java.js
+++ trabajo java/java.js
@@ -1,7 +1,7 @@
 //script sobre el arreglo reduce
 const arreglo = [1, 2, 3, 4, 5];
 const multiplicacion = arreglo.reduce((acumulador, valorActual) => acumulador * valorActual, 0);
-console.log(multiplicacion); 
+console.log(multiplicacion);
 
 
 
@@ -12,46 +12,59 @@ const numerosPares = arreglo.filter((valorActual) => valorActual % 2 === 0);
 console.log(numerosPares); // Imprime: [2, 4, 6]
 
 
+//script sobre los arreglos de personajes,etc.
 
-let personajes = [{
-      nombre: 'Mario',
-      edad: 35
-    },
-    {
-      nombre: 'Link',
-      edad: 30
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    },
-    {
-      nombre: 'Samus Aran',
-      edad: 32
-    }];
-  
-  console.log(personajes);
+let objetoPokemon = [{
+  nombre: 'Nidoking',
+  entrada: 34
+},
+{
+  nombre: 'Nidorino',
+  entrada: 33
+},
+{
+  nombre: 'Nidoran ',
+  entrada: 32
+},
+{
+  nombre: 'Bulbasaur',
+  entrada: 1
+},
+{
+  nombre: 'Ivisaur',
+  entrada: 2
+},
+{
+  nombre: 'Venusaur',
+  entrada: 3
+},
+{
+  nombre: 'Squirtle',
+  entrada: 4
+},
+{
+  nombre: 'Wartortle',
+  entrada: 5
+},
+{
+  nombre: 'Blastoise',
+  entrada: 6
+},
+{
+  nombre: 'Charmander',
+  entrada: 7
+}];
+
+const mostrarPokemons = () => {
+  const contenido = document.getElementById('contenido');
+  objetoPokemon.map(Pokemons => {
+    contenido.innerHTML += `
+      <p class="card mt-5 border-2 border-black ">
+          nombre : ${Pokemons.nombre} <br>
+          entrada : ${Pokemons.entrada} <br>
+      </p>
+      `;
+  })
+}
+
+mostrarPokemons()
